182993ae7ccc00dec5e551fa316b77db19c5cab0
itggot-felix-hornfeldt <felix.hornfeldt@itggot.se>
2018-04-21 11:44:29 +0200

    public map

---
+++ site/public/js/mano_italiano.js
@@ -0,0 +1,27 @@
+// Function that calculates the minimalheight of the main element
+function mainElementMinHeight() {
+    var height = window.innerHeight;
+    var width = window.innerWidth;
+    var minimalHeight;
+    var mainElement = document.querySelector('main');
+    if (width > 800) {
+        minimalHeight = height - 67;
+    } else {
+        minimalHeight = height - 69;
+    }
+    mainElement.style.minHeight = '' + minimalHeight + 'px';
+}
+
+// Function that slide the hamburgermenu and it's content
+function manoItalianoDropdown() {
+    var navigationElement = document.querySelector('nav');
+    var hamburger = document.querySelector('.fa-bars');
+    $(navigationElement).slideToggle(1500, function(){
+        hamburger.classList.toggle('hamburgeractive');
+    });
+}
+
+// function menuFileMainSlide() {
+//     var menuMainElement = document.querySelector(".menumaincontainerbox");
+//     $(menuMainElement).slideToggle(5000, function(){});
+// }
