c021305b57229352fb438d4e3eb8dbe63d3ff4af
itggot-felix-hornfeldt <felix.hornfeldt@itggot.se>
2018-04-27 13:59:10 +0200

    major updates to js, document ready function important

--- site/js/mano_italiano.js
+++ site/js/mano_italiano.js
@@ -1,32 +1,45 @@
-// console.log(document.querySelectorAll('a'));
-
 // Function that calculates the minimalheight of the main element
 function mainElementMinHeight() {
     var height = window.innerHeight;
     var width = window.innerWidth;
     var minimalHeight;
     var mainElement = document.querySelector('main');
-    if (width > 800) {
+    if (width > 880) {
         minimalHeight = height - 67;
     } else {
         minimalHeight = height - 69;
     }
     mainElement.style.minHeight = '' + minimalHeight + 'px'; 
+    // console.log("mainelementminheight");
 }
-
-window.onload = mainElementMinHeight;
 
 // Function that slide the hamburgermenu and it's content
 function manoItalianoDropdown() {
-    // var hamburgerActivator = document.querySelectorAll('.headerhamburgertoggle');
     var navigationElement = document.querySelector('nav');
     var hamburger = document.querySelector('.fa-bars');
     $(navigationElement).slideToggle(1500, function(){
         hamburger.classList.toggle('hamburgeractive');
     });
-    // console.log(navigationElement, hamburger, hamburgerActivator);
+    // console.log("dropdown");
 }
 
 function addToCartAlert() {
     alert("Du kan ej beställa denna produkt just nu. Försök igen senare.");
+    // console.log("alert");
 }
+
+function cartOverlaySlide() {
+    var overlay = document.querySelector('.cartoverlaycontainer');
+    $(overlay).fadeIn(1000);
+    // console.log("overlayslide");
+}
+
+$(function() {
+    var hamburgerActivator = document.querySelector('.headerhamburgertoggle');
+    // var addCartAlert = document.querySelector('.menuitemalert');
+    // addCartAlert.onclick = addToCartAlert;
+    hamburgerActivator.onclick = manoItalianoDropdown;
+    mainElementMinHeight();
+    cartOverlaySlide();
+    // console.log("onclick hamburger");
+})
